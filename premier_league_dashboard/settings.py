121d4163fc91bddb69d95d26910269b26d597b85
Dassoo <federico.dassie@gmail.com>
2025-03-13 10:27:13 +0100

    update

--- premier_league_dashboard/settings.py
+++ premier_league_dashboard/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
