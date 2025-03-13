f6408500da24fedc2c00d11aad846e68493c15b0
Dassoo <federico.dassie@gmail.com>
2025-03-13 10:21:25 +0100

    update

--- premier_league_dashboard/settings.py
+++ premier_league_dashboard/settings.py
@@ -139,8 +139,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
